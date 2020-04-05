48761055cead8b53878fbb7569c7c3bbaf5a50b5
Oliver <rift95@live.se>
2020-04-05 22:11:52 +0200

    f**k d6, d20 is where its at

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const rollDice = require('@olian/dice-roller');
 
 const [__1, __2, ...args] = process.argv;
 const processInput = args.join(' ');
-const input = processInput || 'd6';
+const input = processInput || 'd20';
 
 console.log(input);
 const result = rollDice(input);
